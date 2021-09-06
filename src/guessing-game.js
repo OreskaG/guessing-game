bb093ba38cd491b631ae4f81113dd4fdeb8063d7
OreskaG <Ctepanrfo@gmail.com>
2021-09-06 23:33:56 +0300

    feat: add js code

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.num = Math.round((this.max - this.min) / 2 ) + this.min;
+        return this.num;
     }
 
     lower() {
-
+        this.max = this.num;
     }
 
     greater() {
-
+        this.min = this.num;
     }
 }
 
